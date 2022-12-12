f4ec0baf6ba78ffc8175cbaee9e3fe1775b25256
SuperSecureHuman <supersecurehuman@gmail.com>
2022-12-12 07:36:39 +0530

    Added U-NET image segmentation

---
+++ U-Net-Image_Segmentation/Pytorch/utils.py
@@ -0,0 +1,79 @@
+import torch
+from dataset import image_dataset
+from torch.utils.data import DataLoader
+import torchvision
+from tqdm import tqdm
+
+def save_checkpoint(state, filename="checkpoint.pth"):
+    print("=> Saving checkpoint")
+    torch.save(state, filename)
+
+def load_checkpoint(checkpoint, model):
+    print("=> Loading checkpoint")
+    model.load_state_dict(checkpoint["state_dict"])
+
+def get_loaders(train_dir, train_mask_dir, val_dir, val_mask_dir, batch_size, num_workers=4, pin_memory=True, transform=None):
+    train_ds = image_dataset(train_dir, train_mask_dir, transform=transform)
+
+    train_loader = DataLoader(
+        train_ds,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        shuffle=True,
+        
+    )
+
+    val_ds = image_dataset(val_dir, val_mask_dir, transform=transform)
+    val_loader = DataLoader(
+        val_ds,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        shuffle=True,
+    )
+
+    return train_loader, val_loader
+
+
+def check_accuracy(loader, model, device="cuda"):
+    num_correct = 0
+    num_pixels = 0
+    dice_score = 0
+    model.eval()
+
+    with torch.no_grad():
+        loop = tqdm(loader)
+        for batch_idx, (x, y) in enumerate(loop):
+            x = x.to(device)
+            y = y.to(device).unsqueeze(1)
+            preds = torch.sigmoid(model(x))
+            preds = (preds > 0.5).float()
+            num_correct += (preds == y).sum()
+            num_pixels += torch.numel(preds)
+            dice_score += (2 * (preds * y).sum()) / (
+                (preds + y).sum() + 1e-8
+            )
+
+    print(
+        f"Got {num_correct}/{num_pixels} with acc {num_correct/num_pixels*100:.2f}"
+    )
+    print(f"Dice score: {dice_score/len(loader)}")
+    model.train()
+
+
+def save_predictions(
+    loader, model, folder="saved_images/", device="cuda"
+):
+    model.eval()
+    for idx, (x, y) in enumerate(loader):
+        x = x.to(device=device)
+        with torch.no_grad():
+            preds = torch.sigmoid(model(x))
+            preds = (preds > 0.5).float()
+        torchvision.utils.save_image(
+            preds, f"{folder}/pred_{idx}.png"
+        )
+        torchvision.utils.save_image(y.unsqueeze(1), f"{folder}{idx}.png")
+
+    model.train()
---
+++ U-Net-Image_Segmentation/README.md
@@ -0,0 +1,15 @@
+# Image Segmentation with U-Net architecture
+
+## Introduction
+
+Image segmentation is a very common task in computer vision. It is a problem of finding the boundaries of objects in an image.
+
+Note that the local dataset directory is not included in the repository, because it is around 30GB
+
+I will be using the directory from another drive, and symlinking it here.
+
+I augmented the image seperately, and stored the files seperately, then used it as dataset for training.
+
+Currently, pytorch implementation is done. Will do tensorflow implementation later.
+
+Dataset - https://www.kaggle.com/c/carvana-image-masking-challenge
